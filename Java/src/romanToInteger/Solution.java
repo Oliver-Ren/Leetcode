0c919e064958b66055d5d28cb11474fcf28dfdf2
cr486 <cr486@cornell.edu>
2015-01-15 14:36:09 -0500

    add the code for the roman to int

---
+++ Java/src/romanToInteger/Solution.java
@@ -0,0 +1,38 @@
+public class Solution{
+	public static int romanToInt(String s) {
+		if (s == null || s.length() == 0) return 0;
+		int previousNumber = 1000;
+		int number = 0;
+		int sum = 0;
+		for ( int i = 0; i < s.length(); i++) {
+			switch ( s.charAt(i)) {
+				case 'I': number = 1; break;
+				case 'V': number = 5; break;
+				case 'X': number = 10; break;
+				case 'L': number = 50; break;
+				case 'C': number = 100; break;
+				case 'D': number = 500; break;
+				case 'M': number = 1000; break;
+				default: return 0;
+			}
+
+			if (previousNumber < number) {
+				sum = sum + number - previousNumber - previousNumber;
+			}
+			else sum += number;
+			previousNumber = number;
+		}
+		return number;
+	}
+
+	public static void main(String args[]){
+		String str = "IV";
+		System.out.print(romanToInt(str));
+	}
+}
+
+
+				
+
+
+
