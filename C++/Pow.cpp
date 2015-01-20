33503e67f684e6f7184b46e1f1e17e58a9097d79
cr486 <cr486@cornell.edu>
2015-01-20 00:09:37 -0500

    corrected the pow and the binary version

--- C++/Pow.cpp
+++ C++/Pow.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+double helper(double, int);
+
 //This is the naitve verison of the Pow 
 double powNaive(double x, int n) {
         double result = x;
@@ -21,19 +27,32 @@ double pow(double x, int n) {
 	bool isPos = ( n > 0 ) ? true : false;
 	bool isOdd = ( n % 2 ) ? true : false;
 	double result = 0.00;
-	if ( x == 1.00 || n = 0 ) return 1.00;
-	else if (x == -1.00) return isOdd ? 1.00 : -1.00;
-	if ( isOdd ) result = x * helper( x , n - 1 );
-	else reuslt = helper(x,abs(n));
+	if ( x == 1.00 || n == 0 ) return 1.00;
+	else if (x == -1.00) return isOdd ? -1.00 : 1.00;
+	result = helper(x,abs(n));
 	
-	if ( !isPos && ){
+	if ( !isPos ){
 		assert(result != 0.000);       
 		result = 1/result;
 	}
+	return result;
 }
 
 double helper(double x, int n) {
-	if ( n = 1 ) return x;
+	if ( n == 1 ) return x;
 	else if (x == 0.00) return 0.00;
-	else return helper( x*x, n / 2);
+	else{
+		double v = helper(x , n / 2);
+	       	if (n % 2 == 0)
+			return v*v;
+		else return v*v*x;
+	}
 }
+
+int main( int argc, char * argv[]){
+	double x = 8.88023;
+	int n = 3;
+	double result = pow(x,n);
+	cout << result;
+}
+
