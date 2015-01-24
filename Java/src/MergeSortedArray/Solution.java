fdc0b100efc99b41843b17e3a51d3eb697b1da1e
Oliver-Ren <cr486@cornell.edu>
2015-01-23 21:56:02 -0500

    Modified the solve of the Merge Sorted Array

--- Java/src/MergeSortedArray/Solution.java
+++ Java/src/MergeSortedArray/Solution.java
@@ -4,18 +4,16 @@ public class Solution {
 	public static void merge(int A[], int m, int B[], int n) {
 		int buffer[] = new int[m+n];
 		int i = 0, j = 0;
-		while (i < m && j < n){
-			if (A[i] < B[j]){
+		while ( i < m || j < n){
+			if (j == n || i < m && A[i] < B[j]){
 				buffer[i+j] = A[i];
 				if(i < m) i++;
 			}
-			else{
+			else if (i == m || j < n && A[i] >= B[j] ){
 				buffer[i+j] = B[j];
 				if(j < n) j++;
 			}
 		}
-		
-		for 
 		
 		for (i = 0; i < m+n; i++)
 			A[i] = buffer[i];
@@ -24,11 +22,25 @@ public class Solution {
         
     }
 	
+	public static void mergeReverse(int A[], int m, int B[], int n){
+		int i = m-1, j = n-1;
+		while(i >= 0 || j >= 0){
+			if (j < 0 || i >= 0 && A[i] > B[j]  ){
+				A[i+j+1] = A[i];
+				i--;
+			}
+			else if (i < 0 || j >= 0 && A[i] <= B[j]) {
+				A[i+j+1] = B[j];
+				j--;
+			}
+		}
+	}
+	
 	public static void main(String args[]){
-		int A[] = {1,3,5,7,9,0,0,0,0,0,0,0,0,0,0};
-		int B[] = {2,4,6,8,10,12,14,16};
-		merge(A,5,B,8);
-		for(int i = 0; i < 13; i++)
+		int A[] = {1,2,3,0,0,0};
+		int B[] = {2,5,6};
+		mergeReverse(A,3,B,3);
+		for(int i = 0; i < 6; i++)
 			System.out.println(A[i]);
 	}
 
