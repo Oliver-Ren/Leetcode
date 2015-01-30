e2f718b4301f83893769e93d6a2321e1d854a82b
Oliver-Ren <cr486@cornell.edu>
2015-01-30 00:54:44 -0500

    balanced Binary Tree

---
+++ Java/src/BalancedBinaryTree/Solution.java
@@ -0,0 +1,16 @@
+package BalancedBinaryTree;
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return true;
+    }
+}
