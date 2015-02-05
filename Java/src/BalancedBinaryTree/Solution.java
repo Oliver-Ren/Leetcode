c41951000119a3e65b70c5ec4ed1da73f0fea08c
Oliver-Ren <cr486@cornell.edu>
2015-02-04 23:12:49 -0500

    the balanced binary tree

--- Java/src/BalancedBinaryTree/Solution.java
+++ Java/src/BalancedBinaryTree/Solution.java
@@ -10,7 +10,16 @@ package BalancedBinaryTree;
  * }
  */
 public class Solution {
-    public boolean isBalanced(TreeNode root) {
-        return true;
-    }
+	public boolean isBalanced(TreeNode root) {
+		return level(root) >= 0;
+	}
+
+	public int level(TreeNode root) {
+		if ( root == null ) return 0;
+		int currLeft = level(  root.left);
+		int currRight = level(  root.right );
+		if (currLeft < 0 || currRight < 0 || Math.abs(currLeft - currRight) > 1) 
+			return -1;
+		return Math.max(currLeft, currRight) + 1;
+	}
 }
