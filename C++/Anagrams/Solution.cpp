f67310edeeb210c79e57fb55bffddcde58283558
Oliver-Ren <cr486@cornell.edu>
2015-03-01 17:03:17 -0500

    Restore IP Address

--- C++/Anagrams/Solution.cpp
+++ C++/Anagrams/Solution.cpp
@@ -1,21 +1,70 @@
+/** My solution using stack. */
 class Solution {
 public:
-    vector<string> anagrams(vector<string> &strs) {
-        unordered_map<string, vector<string> > group;
-        for (const auto &s : strs) {
-            string candidate(s);
-            sort(candidate.begin(),candidate.end());
-            group[candidate].push_back(s);
+    string addBinary(string a, string b) {
+        const int lenA = a.size();
+        const int lenB = b.size();
+        
+        int aPointer = lenA - 1;
+        int bPointer = lenB - 1;
+        
+        stack<int> result;
+        int carry = 0;
+        
+        while (aPointer >= 0 || bPointer >= 0) {
+            int aValue = aPointer >= 0 ? a[aPointer] - '0' : 0;
+            int bValue = bPointer >= 0 ? b[bPointer] - '0' : 0;
+            result.push(aValue ^ bValue ^ carry);
+            carry = (aValue + bValue + carry) >> 1;
+            aPointer--;
+            bPointer--;
         }
         
-        vector<string> result;
-        for (const auto &kv : group) {
-            if (kv.second.size() > 1) {
-                for (const auto &word : kv.second) {
-                    result.push_back(word);
-                }
-            }
+        if (carry == 1) result.push(carry);
+        
+        string sum;
+        const int digits = result.size();
+
+		for (int i = 0; i < digits; i++) {
+            char digit = '0' + result.top();
+            sum.push_back(digit);
+            result.pop();
         }
+        
+        return sum;
+        
+        
+    }
+};
+
+
+
+/** My solution which does not use stack */)
+class Solution2 {
+public:
+    string addBinary(string a, string b) {
+        const int lenA = a.size();
+        const int lenB = b.size();
+        
+        int aPointer = lenA - 1;
+        int bPointer = lenB - 1;
+        
+        string result;
+        int carry = 0;
+        
+        while (aPointer >= 0 || bPointer >= 0) {
+            int aValue = aPointer >= 0 ? a[aPointer] - '0' : 0;
+            int bValue = bPointer >= 0 ? b[bPointer] - '0' : 0;
+            result.push_back(aValue ^ bValue ^ carry + '0');
+            carry = (aValue + bValue + carry) >> 1;
+            aPointer--;
+            bPointer--;
+        }
+        
+        if (carry == 1) result.push_back('1');
+        reverse(result.begin(),result.end());
         return result;
+        
+        
     }
 };
