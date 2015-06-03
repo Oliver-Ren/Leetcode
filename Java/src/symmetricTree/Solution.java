eed248bef1c762efb9afda4ee18f64eadd5196dc
Chengxiang Ren <cr486@cornell.edu>
2015-06-03 20:40:12 +0800

    the iterative version of the SymmetricTree

--- Java/src/symmetricTree/Solution.java
+++ Java/src/symmetricTree/Solution.java
@@ -8,6 +8,7 @@
  * }
  */
 public class Solution {
+	/* this is the recursive version of the solution. */
     public boolean isSymmetric(TreeNode root) {
        if (root == null) {
 		   return true;
@@ -28,5 +29,49 @@ public class Solution {
 
 		return (helper(left.left, right.right) && 
 				helper(left.right, right.left));
+	}
+
+	/* This is the iterative version of the solution. */
+	public boolean isSymmetricIter(TreeNode root) {
+		/* the condition where the root does not exist. */
+		if (root == null) {
+			return true;
+		}
+
+		TreeNode left = root.left;
+		TreeNode right = root.right;
+		Deque<TreeNode> level1 = new LinkedList<TreeNode>();
+		Deque<TreeNode> level2 = new LinkedList<TreeNode>();
+
+		level1.addFirst(root.left);
+		level1.addLast(root.right);
+		
+		while(!level1.isEmpty()) {	
+			while (!level1.isEmpty()) {
+				
+				left = level1.pollFirst();
+				right = level1.pollLast();
+				if (left == null && right == null) {
+					continue;
+				} else if (left == null || right == null
+						|| left.val != right.val) {
+					return false;
+				} else if (left.val == right.val) {
+					level2.addFirst(left.left);
+					level2.addLast(right.right);
+					level2.addFirst(left.right);
+					level2.addLast(right.left);
+				}
+
+			}
+
+			Deque<TreeNode> temp = level1;
+			level1 = level2;
+			level2 = temp;
+		}
+
+		return true;
+	}
+			
 
 }
