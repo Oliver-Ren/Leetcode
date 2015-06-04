b52be82ff8de333b16159a11c3645ca4f4eb5438
Chengxiang Ren <cr486@cornell.edu>
2015-06-04 18:03:35 +0800

    the itertive version of the FindMinimumin rotate Sorted Array$ git push

--- Java/src/FindMinimuminRotatedSortedArray/Solution.java
+++ Java/src/FindMinimuminRotatedSortedArray/Solution.java
@@ -17,4 +17,19 @@ public class Solution {
         }
         return 0;
     }
+
+	/* below is the iterative version. */
+	public int findMinIter(int[] nums) {
+		int left = 0;
+		int right = nums.length - 1;
+		
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			if (nums[left] < nums[right]) return nums[left];
+			if (nums[mid] >= nums[left]) left = mid + 1;
+			else right = mid;
+		}
+
+		return num[left];
+
 }
--- Java/src/findMinimumInRotatedSortedArrayII/Solution.java
+++ Java/src/findMinimumInRotatedSortedArrayII/Solution.java
@@ -1,4 +1,61 @@
 public class Solution {
 	public int findMin(int[] nums) {
 		// The condition when the array is null
-		
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+
+		int firstElement = nums[0];
+		boolean isFound = false;
+		// The condition where the array is not rotated.
+		if (nums[0] < nums[nums.length - 1] || nums.length == 1) {
+			return nums[0];
+		}
+
+
+		// The condition where the array is rotated.
+		int leftBound = 0;
+		int rightBound = nums.length - 1;
+
+
+		/* We should firstly consider the condition
+		   where the left most element and the right 
+		   most element are the same, the target might
+		   be hidden between the left most and the 
+		   right most element. 
+
+		   for eample 3333333313. */
+		if (nums[0] == nums[nums.length - 1]) {
+			while (nums[leftBound] == nums[rightBound]) {
+				rightBound--;
+				if (rightBound == 0) {
+					return nums[0];
+				}
+			}
+
+		}
+
+		int pointer = (rightBound - leftBound) / 2;
+
+		while (true) {
+			if (nums[pointer] > nums[pointer + 1]) {
+				return nums[pointer + 1];
+			}
+
+			if (nums[pointer] < nums[rightBound]) {
+				leftBound = pointer + 1;
+			} else {
+				rightBound = pointer - 1;
+
+			}
+
+			pointer = (rightBound - leftBound) / 2;
+			if (rightBound - leftBound <= 1) {
+				return nums[leftBound] < nums[rightBound] ? 
+					nums[leftBound] : nums[rightBound];
+			}
+		}
+	}
+}
+
+
