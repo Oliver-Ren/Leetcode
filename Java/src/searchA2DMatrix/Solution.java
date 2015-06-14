f7590e9899e0c8e408927f68db7fead355e0469c
Chengxiang Ren <cr486@cornell.edu>
2015-06-14 21:23:05 +0800

    AC code for search a 2D matrix

--- Java/src/searchA2DMatrix/Solution.java
+++ Java/src/searchA2DMatrix/Solution.java
@@ -1,17 +1,29 @@
+import java.util.Arrays;
+
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 		// The null condition.
 		if (matrix == null || matrix[0] == null) {
-			return -1;
+			return false;
 		}
 
 		/* find the appropriate row entry for the matrix.*/
 		int smallRow = 0;
-		int largeRow = matrix.length;
+		int largeRow = matrix.length - 1;
 		int entry = 0; //in fact the middle index of the column
 
-		while (smallRow < largeRow) {
+		while (smallRow <= largeRow) {
 			entry = (smallRow + largeRow) / 2;
+			
+			if (largeRow - smallRow <= 1) {
+			    if (matrix[largeRow][0] > target) {
+			        entry = smallRow;
+			    } else {
+			        entry = largeRow;
+			    }
+			    
+			    break;
+			}
 
 			if (matrix[entry][0] < target) {
 				smallRow = entry;
@@ -19,16 +31,29 @@ public class Solution {
 				largeRow = entry - 1;
 			} else {
 				break;
-			}
-
-			Arrays.
+			}	
 
 		}
+		
+		
+		int result = Arrays.binarySearch(matrix[entry], target);
+		return result < 0 ? false : true;
+	}
+
+	public static void main(String[] args) {
+		Solution solu = new Solution();
+		int[][] testArray = new int[1][1];
+		testArray[0][0] = 1;
+		System.out.println(solu.searchMatrix(testArray, 2));
+	}
+
+
+}
+	
+	
 
 		
 				
 
 			
 
-    }
-}
