acaf015fa2b2b96175d92e1073bce3ce552cee23
Chengxiang Ren <cr486@cornell.edu>
2015-06-26 17:29:26 +0800

    combination

---
+++ Java/src/combinations/Solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+	public List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		
+		for (int i = 0; i < n; i++) {
+			
+
+		return result;
+
+	}
+
+	private List<Integer> genera(
+
+	public static void main(String [] args) {
+		Solution test = new Solution();
+		
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+	}
+}
