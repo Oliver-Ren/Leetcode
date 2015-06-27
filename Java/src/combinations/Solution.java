42e17b78a802b1a8860d03709bab07287d2e1020
Chengxiang Ren <cr486@cornell.edu>
2015-06-27 23:51:54 +0800

    AC Java code for Combinations

--- Java/src/combinations/Solution.java
+++ Java/src/combinations/Solution.java
@@ -3,15 +3,25 @@ import java.util.*;
 public class Solution {
 	public List<List<Integer>> combine(int n, int k) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
-		
-		for (int i = 0; i < n; i++) {
-			
-
+		List<Integer> path = new ArrayList<Integer>();
+		dfs(n, k, 1, 0, path, result);
 		return result;
-
 	}
 
-	private List<Integer> genera(
+	private void dfs(int n, int k, int start, int currPos, 
+			List<Integer> path, List<List<Integer>> result) {
+		if (currPos == k) {
+			result.add(new ArrayList(path));
+			return;
+		}
+
+		for (int i = start; i <= n; i++) {
+			path.add(i);
+			dfs(n, k, i + 1, currPos + 1, path, result);
+			path.remove(path.size() - 1);
+		}
+	}
+
 
 	public static void main(String [] args) {
 		Solution test = new Solution();
