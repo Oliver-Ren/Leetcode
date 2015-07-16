f3cb743f948bd5beaa0d62892d701511474cac9f
Chengxiang Ren <cr486@cornell.edu>
2015-07-16 10:24:31 +0800

    MIT hackmem solution for the Number of 1 Bits

--- Java/src/numberOf1Bits/Solution.java
+++ Java/src/numberOf1Bits/Solution.java
@@ -9,4 +9,12 @@ public class Solution {
         return count;
         
     }
+
+    public int hammingWieght1(int n) {
+        int tmp;
+
+        tmp = n - ((n >> 1) & 033333333333)
+            - ((n >> 2) & 011111111111);
+        return ((tmp + (tmp >> 3) & 030707070707) % 63;
+    }
 }
