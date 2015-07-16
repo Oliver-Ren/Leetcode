f3f1880195aa0889dd3ca7fc2bc6aef5f618084d
Chengxiang Ren <cr486@cornell.edu>
2015-07-17 07:29:30 +0800

    AC code for binary tree postorder traversal which keeps the structure of the tree

---
+++ Java/src/BinaryTreePostorderTraversal/Solution_Iterative3.java
@@ -0,0 +1,41 @@
+package BinaryTreePostorderTraversal;
+
+import java.util.List;
+import java.util.Stack;
+import java.util.ArrayList;
+
+/** The iterative version of the postroder traversal.
+  * This solutio does not change the structure of the tree.
+  * The time complexity is O(n), the space complexity
+  * is O(n). 
+  * Status: Accepted.
+  */
+
+public class Solution_Iterative3 {
+	public static List<Integer> reorderTraversal(TreeNode root){
+		List<Integer> result = new ArrayList<Integer> ();
+        Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+        
+        TreeNode curr = root;
+        TreeNode prev = root;
+
+        while (curr != null) {
+
+            if (curr.left != null && prev != curr.left && prev != curr.right) {
+                nodeStack.push(curr);
+                prev = curr;
+                curr = curr.left;
+            } else if (curr.right != null && prev != curr.right) {
+                nodeStack.push(curr);
+                prev = curr;
+                curr = curr.right;
+            } else {
+                result.add(curr.val);
+                prev = curr;
+                curr = nodeStack.empty() ? null : nodeStack.pop();
+            }
+        }
+
+        return result;
+    }
+}
