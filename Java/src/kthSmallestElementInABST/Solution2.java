ea8522170d228db54dbcd82f7a4f92ad013906de
Chengxiang Ren <cr486@cornell.edu>
2015-07-22 22:09:42 +0800

    dfs version of kth smallest elemtn in a BST

---
+++ Java/src/kthSmallestElementInABST/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/* this is the recursive inorder traversal
+ * solution.
+ * the time complexity is O(logn + k)
+ * the space complexity is O(logn)
+ * Status: Accpeted.
+ */
+
+public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+
+    }
+}
+
