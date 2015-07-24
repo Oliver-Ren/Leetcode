ddc0167cdc077f247eac367abde67e6626b2febe
Chengxiang Ren <cr486@cornell.edu>
2015-07-24 22:24:38 +0800

    AC code for minimum depth of binary tree

---
+++ Java/src/minimumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,35 @@
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MAX_VALUE; 
+
+        if (root.left != null) {
+            left = minDepth(root.left);
+        }
+
+        if (root.right != null) {
+            right = minDepth(root.right);
+        }
+
+        return Math.min(left, right) + 1;
+
+    }
+}
