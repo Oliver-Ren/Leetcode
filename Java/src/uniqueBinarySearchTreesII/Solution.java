7c79164116c77b9f25d1a82b8f134c2946470a39
Chengxiang Ren <cr486@cornell.edu>
2015-07-31 19:26:58 +0800

    AC code for unique binary search trees

--- Java/src/uniqueBinarySearchTreesII/Solution.java
+++ Java/src/uniqueBinarySearchTreesII/Solution.java
@@ -8,37 +8,40 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+/* recursive calls.
+ * Status: Accepted, 348ms.
+ */ 
+
 public class Solution {
     public List<TreeNode> generateTrees(int n) {
-        List<TreeNode> result = new LinkedList<TreeNode>();
-        for (int i = 1; i <= n; i++) {
-            TreeNode root = new TreeNode(i);
-            makeBST(root);
-            
-        }
-        
+        return makeBST(1, n);
     }
 
-    private void makeBST(TreeNode root, int left, int mid, int right, boolean isLeft) {
-        if (mid <= left && isLeft) {
-            makeBST(root, false);
+    private List<TreeNode> makeBST(int left, int right) {
+        List<TreeNode> cand = new LinkedList<TreeNode>();
+        if (left > right) {
+            cand.add(null);
+            return cand;
         }
 
-
-        if (!isLeft) {
-            makeBST(
-        } else {
-
+        
 
         for (int mid = left; mid <= right; mid++) {
-            TreeNode newRoot = new TreeNode(mid);
-            root.left = newRoot;
-            if (mid > left) {
-                makeBST(newRoot, left, mid - 1, true);
-            } else {
-                makeBST(root, mid + 1, right, false);
+            List<TreeNode> leftCand = makeBST(left, mid - 1);
+            List<TreeNode> rightCand = makeBST(mid + 1, right);
+
+            for (TreeNode l : leftCand) {
+                for (TreeNode r : rightCand) {
+                    TreeNode root = new TreeNode(mid);
+                    root.left = l;
+                    root.right = r;
+                    cand.add(root);
+                }
+            }
         }
 
-
-
+        return cand;
+    }
 }
+
