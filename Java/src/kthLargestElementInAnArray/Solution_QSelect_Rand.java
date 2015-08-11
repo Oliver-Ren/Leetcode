a2f3ab23de979167d0c4e4e54ac8e44d14098954
Chengxiang Ren <cr486@cornell.edu>
2015-08-11 23:47:19 +0800

    quick select reimplemented for kth largest number in an array

---
+++ Java/src/kthLargestElementInAnArray/Solution_QSelect_Rand.java
@@ -0,0 +1,63 @@
+/** This solution is the randomized
+  * quick select..
+  * the Time complexity is: O(n);
+  * space complexity is: O(logn);
+  * Status: 
+  */
+
+public class Solution_QSelect {
+    public int findKthLargest(int[] nums, int k) {
+        shuffle(nums);
+        int lo = 0;
+        int hi = nums.length - 1;
+        int mid = 0;
+        while (true) {
+            mid = partition(nums, lo, hi);
+            if (mid == nums.length - k) {
+                break;
+            } else if (mid > nums.length - k) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return num[mid];
+    }
+
+    private int partition(int nums, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        
+        while (true) {
+            while (i < hi && nums[i++] < nums[lo]);
+            while (j > 0 && nums[--j] > nums[lo]);
+
+            if (i > j) {
+                break;
+            } else {
+                exch(nums, i, j);
+            }
+        }
+
+        exch(nums, lo, j);
+        reutrn j;
+    }
+
+    private void exch(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private shuffle(int[] nums) {
+        Random random = new Random();
+        for (int i = 0; i < nums.length; i++) {
+            int r = random.nextInt(i + 1);
+            exch(nums, i, r);
+        }
+    }
+            
+}
+
+        
