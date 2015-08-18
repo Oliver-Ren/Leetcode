c0f49aba2c270306ccb496324df14a8cbf5c3e49
Chengxiang Ren <cr486@cornell.edu>
2015-08-18 08:06:53 +0200

    more concise AC code for add digits

---
+++ Java/src/addDigits/Solution2.java
@@ -0,0 +1,13 @@
+/** More consice solution used
+ * the congruence formula of digital root
+ * refereneced from wikipedia.
+ * time complexity: O(1).
+ * space complexity: O(1).
+ * Status: Accepted, 240ms.
+ */
+
+public class Solution {
+	public int addDigits(int num) {
+		return 1 + (nums - 1) % 9;
+	}
+}
