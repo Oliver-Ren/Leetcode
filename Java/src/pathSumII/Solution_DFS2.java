0979c07b5a7eeba3b8bf6b449d0703bcb422ed2a
Chengxiang Ren <cr486@cornell.edu>
2015-09-15 15:18:56 -0400

    more concise AC code for path Sum II

--- Java/src/pathSumII/Solution_DFS2.java
+++ Java/src/pathSumII/Solution_DFS2.java
@@ -9,51 +9,45 @@
  */
 
 /*
- * My first attempt using DFS and backtracking.
+ * My second attempt using DFS and backtracking.
+ * I will try to make the code more concise by making use of the propertyies
+ * of recursive.
  * Time complexity: O(n) where n is the number of nodes.
  * Space complexity: O(logn) in average case, O(n) worst.
- * Status: accepted, 348ms.
+ * Status: accepted, 296ms.
  */
 
 public class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-
-        // Dealing with corner cases.
-        if (root == null) {
-            return result;
-        }
-
         List<Integer> path = new ArrayList<Integer>();
-
-        TreeNode dummyRoot = new TreeNode(0);
-        dummyRoot.left = root;
-        dfs(result, path, sum, dummyRoot);
+        dfs(result, path, sum, root);
         return result;
     }
 
     // DFS method for recursively and backtrack;
     private void dfs(List<List<Integer>> result, List<Integer> path, int sum, TreeNode root) {
+        // return if the node is empty
+        if (root == null) {
+            return;
+        }
+
         // deduce the value in the root from sum.
         sum -= root.val;
 
         if (root.left == null & root.right == null & sum == 0) {
+            path.add(root.val);
             result.add(new ArrayList<Integer>(path));
+            path.remove(path.size() - 1);
             return;
         }
 
-        if (root.left != null) {
-            path.add(root.left.val);
-            dfs(result, path, sum, root.left); 
-            path.remove(path.size() - 1);
-        }
-
-        if (root.right != null) {
-            path.add(root.right.val);
-            dfs(result, path, sum, root.right); 
-            path.remove(path.size() - 1);
-        }
-
+        // add current to path.
+        path.add(root.val);
+        dfs(result, path, sum, root.left);
+        dfs(result, path, sum, root.right);
+        // remove current to path.
+        path.remove(path.size() - 1);
     }
 
 }
