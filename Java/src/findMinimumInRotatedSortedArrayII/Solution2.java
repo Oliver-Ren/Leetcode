101a5a2ecf7ccaae961b102651899c07c2a403e8
Chengxiang Ren <cr486@cornell.edu>
2015-09-25 08:57:48 -0400

    iterative solution for find minimum in rotated sorted array

---
+++ Java/src/findMinimumInRotatedSortedArrayII/Solution2.java
@@ -0,0 +1,26 @@
+/**
+ * Revisit the problem.
+ * idea: Binary search.
+ * Time complexity: O(logn)
+ * Space compleixty: O(logn).
+ * Status: DNF.
+ */
+public class Solution3 {
+    public int findMin(int[] nums) {
+        // precondition: nums is not null
+        if (nums == null) {
+            throw new NullPointerException();
+        }
+
+        int lo = 0;
+        int hi = nums.length;
+
+        while (lo + 2 < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > nums[hi - 1]) lo = mid + 1;
+            else hi = mid + 1;
+        }
+
+        return Math.min(nums[lo], nums[hi - 1]);
+    }
+}
