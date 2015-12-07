976837b407efe37d63a885b3e89270dbc1d64649
Chengxiang Ren <cr486@cornell.edu>
2015-12-07 01:01:09 -0500

    DP solution for Jump Game

--- Java/src/JumpGame/Solution.java
+++ Java/src/JumpGame/Solution.java
@@ -1,5 +1,32 @@
-package JumpGame;
 
+/**
+ * The dynamic programming soluiton.
+ * time complexity: O(n^2);
+ * space complexity: O(n);
+ * Status; Accepted;
+ */
 public class Solution {
-	
+    public boolean canJump(int[] nums) {
+        // precondition: nums is not null
+        if (nums == null) throw new NullPointerException();
+        int len = nums.length;
+
+        boolean[] opt = new boolean[len];
+
+        // init
+        opt[0] = true;
+
+        for (int i = 1; i < len; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] + j >= i) {
+                    opt[i] = true;
+                    break;
+                }
+            }
+            if (opt[i] == false) return false;
+        }
+
+        return true;
+    }
 }
+        
