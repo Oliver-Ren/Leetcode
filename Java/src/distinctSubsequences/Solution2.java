d306d3e1a8252c0dfc03efd6eb775ecf399a554d
Chengxiang Ren <cr486@cornell.edu>
2015-12-10 23:08:19 -0500

    DP solution with sliding array for distinct subsequences

--- Java/src/distinctSubsequences/Solution2.java
+++ Java/src/distinctSubsequences/Solution2.java
@@ -1,5 +1,5 @@
 /**
- * Dynamic Programming Solution.
+ * An optimizaed Dynamic Programming Solution.
  * State: f(i, j) represents max number of distinct subsequences
  * betwenn substring of s from 0 to i and substring of t from 0 to j.
  * Function: f(i, j) = f(i - 1, j - 1) + f(i - 1, j), if s[i] == t[j]; 
@@ -7,9 +7,9 @@
  * Initialization: f(i, 0) = 1, f(0, j) = 0;
  * Answer: f(m, n);
  *
- * Time ComplexitY: O(n^2);
- * Space Complexity: O(n^2);
- * Status: Accepted, 25ms.
+ * Time ComplexitY: O(m * n);
+ * Space Complexity: O(n);
+ * Status: Accepted, 15ms.
  */
 public class Solution {
     public int numDistinct(String s, String t) {
@@ -19,34 +19,19 @@ public class Solution {
         int m = s.length();
         int n = t.length();
 
-        int[][] f = new int[m + 1][n + 1];
+        int[] f = new int[n + 1];
         // init
-        for (int i = 0; i <= m; i++) {
-            f[i][0] = 1;
-        }
+        f[0] = 1;
 
         for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= i && j <= n; j++) {
+            for (int j = Math.min(i, n); j >= 1; j--) {
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
-                    f[i][j] = f[i - 1][j - 1] + f[i - 1][j];
-                } else {
-                    f[i][j] = f[i - 1][j];
+                    f[j] = f[j-1] + f[j];
                 }
             }
         }
 
-        for (int i = 0; i<= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                System.out.print(" " + f[i][j]);
-            }
-            System.out.println();
-        }
-
-        return f[m][n];
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        s.numDistinct("ddd", "dd");
+        return f[n];
     }
 }
+
