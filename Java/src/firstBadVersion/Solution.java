becbf03296a600df328bc3fbf23d8e7ac17d0ac1
Chengxiang Ren <cr486@cornell.edu>
2016-01-20 14:19:55 -0500

    AC solution for first bad version

---
+++ Java/src/firstBadVersion/Solution.java
@@ -0,0 +1,24 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int lo = 0;
+        int hi = n;
+
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (isBadVersion(mid)) {
+                hi = mid;
+            } else {
+                lo = mid;
+            }
+        }
+
+        if (isBadVersion(lo)) {
+            return lo;
+        }
+
+        return hi;
+    }
+}
